904f3f1dcb512f9d9bae94bd2e3e8306e012cdd8
devsobirov <devsobirov@gmail.com>
2023-06-16 08:08:46 +0500

    schedule status testing job added [3]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Jobs\CheckClosedTasks;
 use App\Jobs\CheckExpiredTasks;
+use App\Jobs\ScheduleStatusLoggerJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -17,7 +18,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->job(CheckExpiredTasks::class)->dailyAt('07:00');
         $schedule->job(CheckClosedTasks::class)->dailyAt('07:05');
-        $schedule->job(CheckClosedTasks::class)->everyMinute();
+        $schedule->job(ScheduleStatusLoggerJob::class)->everyMinute();
     }
 
     /**
