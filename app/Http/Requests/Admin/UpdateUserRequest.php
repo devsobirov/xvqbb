331b82de553c790114a0cb4e7574491f1c446a28
devsobirov <devsobirov@gmail.com>
2023-07-04 02:38:42 +0500

    user password update error fix

--- app/Http/Requests/Admin/UpdateUserRequest.php
+++ app/Http/Requests/Admin/UpdateUserRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Admin;
 
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 
 class UpdateUserRequest extends FormRequest
